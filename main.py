b10f7b23f2be5ba8b27ed0e1e080dab18dd224e7
Lion Grabau <liongrabau@xing.com>
2024-03-21 01:12:56 +0100

    neo4j connect

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.neo4j import Database
+
+if __name__ == "__main__":
+    Database()
--- src/neo4j.py
+++ src/neo4j.py
@@ -1,9 +1,11 @@
 from neo4j import GraphDatabase
 
+
 class Database:
-    
+
     def __init__(self) -> None:
-        self.URI = "neo4j://localhost"
+        self.URI = "bolt://localhost:7687"
         self.AUTH = ("neo4j", "password")
         with GraphDatabase.driver(self.URI, auth=self.AUTH) as driver:
-            print(driver.verify_connectivity())
+            with driver.session() as session:
+                print(session.run("RETURN datetime()").single()[0])
